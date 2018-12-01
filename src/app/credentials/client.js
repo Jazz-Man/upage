e49b6b48dfef91ddc67bbc20e76a4fc53d28961d
Vasyl Sokolyk <vsokolyk@gmail.com>
2018-12-01 21:44:48 +0200

    init firebase cred

---
+++ src/app/credentials/client.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: "AIzaSyAQv-XvUZ5kunxY3H0p763CxFp4fDkxSYE",
+  authDomain: "upages-220009.firebaseapp.com",
+  databaseURL: "https://upages-220009.firebaseio.com",
+  projectId: "upages-220009",
+  storageBucket: "upages-220009.appspot.com",
+  messagingSenderId: "473305719776"
+};
