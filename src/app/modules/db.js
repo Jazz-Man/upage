dae60ccfd51ce40a544bafad529aa872a28dd057
Vasyl Sokolyk <vsokolyk@gmail.com>
2018-12-01 21:46:46 +0200

    updated db module

--- src/app/modules/db.js
+++ src/app/modules/db.js
@@ -1,15 +1,10 @@
+import credentials from './../credentials/client';
+
 export default async function loadDB() {
   const firebase = await import("firebase");
 
   try {
-    firebase.initializeApp({
-      apiKey: "AIzaSyAQv-XvUZ5kunxY3H0p763CxFp4fDkxSYE",
-      authDomain: "upages-220009.firebaseapp.com",
-      databaseURL: "https://upages-220009.firebaseio.com",
-      projectId: "upages-220009",
-      storageBucket: "upages-220009.appspot.com",
-      messagingSenderId: "473305719776"
-    });
+    firebase.initializeApp(credentials);
   } catch (err) {
     // we skip the "already exists" message which is
     // not an actual error when we're hot-reloading
