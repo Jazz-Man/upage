6987a35a81aa33321190f324a730d83b5a5ccaf2
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-06-07 20:55:17 +0300

    setup base Document

--- src/app/pages/_document.tsx
+++ src/app/pages/_document.tsx
@@ -1,24 +1,27 @@
 import Document, { Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheet } from "styled-components";
 import React from "react";
 
 export default class MyDocument extends Document {
-
   render() {
     const sheet = new ServerStyleSheet();
-    const main = sheet.collectStyles(<Main />)
-    const styleTags = sheet.getStyleElement()
+    const main = sheet.collectStyles(<Main />);
+    const styleTags = sheet.getStyleElement();
     return (
       <html>
-      <title>My page</title>
+        <title>My page</title>
         <Head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"/>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
           {styleTags}
         </Head>
         <body>
-        {main}
+          {main}
           <NextScript />
+          <div id="modal-root" />
         </body>
       </html>
     );
